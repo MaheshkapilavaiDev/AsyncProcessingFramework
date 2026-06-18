b6b50f1515341d5704bd19a4394564c75e9ececd
Mahesh Kumar <maheshkapilavai3230@gmail.com>
2026-06-18 09:47:38 +0530

    Swagger api and code refactor

--- src/main/java/com/asyncprocessingframework/exception/GlobalExceptionHandler.java
+++ src/main/java/com/asyncprocessingframework/exception/GlobalExceptionHandler.java
@@ -1,5 +1,38 @@
 package com.asyncprocessingframework.exception;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.asyncprocessingframework.entity.ErrorResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+
+		ErrorResponse response = new ErrorResponse();
+
+		response.setTimestamp(LocalDateTime.now());
+		response.setMessage(ex.getMessage());
+		response.setStatus(HttpStatus.BAD_REQUEST.value());
+
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+
+		ErrorResponse response = new ErrorResponse();
+
+		response.setTimestamp(LocalDateTime.now());
+		response.setMessage(ex.getMessage());
+		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
